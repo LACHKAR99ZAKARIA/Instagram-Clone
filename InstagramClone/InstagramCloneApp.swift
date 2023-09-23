133f39dca6d8b4432713293deed6c78adfb56072
LACHKAR99ZAKARIA <lachkar99zaakria@gmail.com>
2023-09-23 14:15:33 +0100

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Zakarai Lachkar on 23/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
